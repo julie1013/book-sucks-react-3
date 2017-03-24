9df97d1741ed9800a7f3612a143232943ba51d58
julie1013 <julie1380@verizon.net>
2017-03-23 22:26:43 -0400

    Added BookDetails component files
    Added BookDetails component files and set up layout for component

---
+++ src/Components/ThisBookSucks/BookDetails/BookDetails.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './BookDetails.css';
+
+export default class BookDetails extends Component {
+  render() {
+    return (
+      <div className="book-details">
+        BOOK DETAILS
+      </div>
+    )
+  }
+}
--- src/Components/ThisBookSucks/ThisBookSucks.js
+++ src/Components/ThisBookSucks/ThisBookSucks.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './ThisBookSucks.css';
 import { ToRead } from './ToRead';
+import { BookDetails } from './BookDetails';
 //looks for index.js in ToRead directory
 
 export default class ThisBookSucks extends Component {
@@ -8,6 +9,7 @@ export default class ThisBookSucks extends Component {
     return (
       <div className="main-app-area">
       <ToRead />
+      <BookDetails />
       </div>
     )
   }
