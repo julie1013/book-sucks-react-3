ce8463c573c5cf90b09f304007be647d7f7b7d06
julie1013 <julie1380@verizon.net>
2017-03-24 19:35:19 -0400

    Formatted temporary login button placeholder component
    Formatted temporarily login button placeholder component. Added CSS

--- src/Components/SignUpIn/SignUpIn.js
+++ src/Components/SignUpIn/SignUpIn.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-
+import './SignUpIn.css';
 
 export default class SignUpIn extends Component {
   render(){
     return (
-      <div className="temp-sign-up" onClick={()=>this.props.loginFunc()}>TEMPORARY GO TO APP!</div>
+      <div className="temp-sign-up"
+        onClick={()=>
+          this.props.loginFunc()}>
+          LOGIN PLACEHOLDER
+      </div>
     )
   }
 }
