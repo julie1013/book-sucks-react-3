dedf8c4916c5fc4c7f9bf5e78cc8cba7af5d6d21
julie1013 <julie1380@verizon.net>
2017-03-26 15:34:39 -0400

    Added auth.js, wrote login code
    Added auth.js and wrote the login code

--- src/Components/Landing/SignUpIn/SignUpIn.js
+++ src/Components/Landing/SignUpIn/SignUpIn.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import './SignUpIn.css';
+import { logIn } from './auth';
 
 export default class SignUpIn extends Component {
+  constructor(){
+    super();
+    this.handleSubmit=this.handleSubmit.bind(this);
+    //telling this function that this is SignUpIn Component
+    //binds handleSubmit to SignUpIn Component (context)
+  }
+  handleSubmit(event){
+    event.preventDefault();
+    let formPayload = {
+      email: event.target.children[0].value,
+      password: event.target.children[1].value
+    }
+    logIn(formPayload)
+    // this.setState;
+  }
+
   render(){
     return (
-      <div className="temp-sign-up"
-        onClick={()=>
-          this.props.loginFunc()}>
-          LOGIN PLACEHOLDER
+      <div className="temp-sign-up">
+          <form onSubmit={this.handleSubmit}>
+            <input type="text"></input>
+            <input type="password"></input>
+            <button type="submit">Sign In</button>
+          </form>
       </div>
-    )
+    );
   }
 }
