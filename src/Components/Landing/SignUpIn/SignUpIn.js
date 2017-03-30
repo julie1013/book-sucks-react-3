51114f0374bafbf21c62b71ea47109b33ee86628
Matt T <code.matthew.thompson@gmail.com>
2017-03-30 12:45:41 -0400

    fixes the promise chain example we made that went missing somehiw

--- src/Components/Landing/SignUpIn/SignUpIn.js
+++ src/Components/Landing/SignUpIn/SignUpIn.js
@@ -1,33 +1,40 @@
-import React, { Component } from 'react';
-import './SignUpIn.css';
-import { logIn } from './auth';
+import React, { Component } from 'react'
+import './SignUpIn.css'
+import { logIn } from './auth'
 
 export default class SignUpIn extends Component {
-  constructor(){
-    super();
-    this.handleSubmit=this.handleSubmit.bind(this);
-    //telling this function that this is SignUpIn Component
-    //binds handleSubmit to SignUpIn Component (context)
+  constructor () {
+    super()
+    this.handleSubmit = this.handleSubmit.bind(this)
+    // telling this function that this is SignUpIn Component
+    // binds handleSubmit to SignUpIn Component (context)
   }
-  handleSubmit(event){
-    event.preventDefault();
+  handleSubmit (event) {
+    event.preventDefault()
     let formPayload = {
       email: event.target.children[0].value,
       password: event.target.children[1].value
     }
     logIn(formPayload)
+    .then((response) => {
+      if (response.status === 201) {
+        this.props.loginFunc()
+      } else {
+        alert('login failed!')
+      }
+    })
     // this.setState;
   }
 
-  render(){
+  render () {
     return (
-      <div className="temp-sign-up">
-          <form onSubmit={this.handleSubmit}>
-            <input type="text"></input>
-            <input type="password"></input>
-            <button type="submit">Sign In</button>
-          </form>
+      <div className='temp-sign-up'>
+        <form onSubmit={this.handleSubmit}>
+          <input type='text' />
+          <input type='password' />
+          <button type='submit'>Sign In</button>
+        </form>
       </div>
-    );
+    )
   }
 }
