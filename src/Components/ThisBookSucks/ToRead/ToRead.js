445e30153d59a2a5e5d2e0669cf5b45a934a9215
julie1013 <julie1380@verizon.net>
2017-04-02 15:55:54 -0400

    User can now see number of books left on to-read list

--- src/Components/ThisBookSucks/ToRead/ToRead.js
+++ src/Components/ThisBookSucks/ToRead/ToRead.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import './ToRead.css';
+import { getUserInfo } from '../getUserInfo';
 
 export default class ToRead extends Component {
+  constructor(){
+    super();
+    this.state = {numBooks: undefined}
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+
+  componentDidMount() {
+    getUserInfo()
+    .then((response)=>{
+      return response.json();
+    })
+    .then((json)=>{
+      this.setState({numBooks: json.info.numberOfReads})
+    })
+  }
+
   render() {
     return (
       <div className="to-read">
-      TO READ
+      {this.state.numBooks + ' books to read!'} 
       </div>
     )
   }
