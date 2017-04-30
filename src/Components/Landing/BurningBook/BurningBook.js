bad48692e2023f9a03293314730b88d85601bf0f
julie1013 <julie1380@verizon.net>
2017-04-30 14:35:08 -0400

    Fixed burning book image, which wasn't showing up

--- src/Components/Landing/BurningBook/BurningBook.js
+++ src/Components/Landing/BurningBook/BurningBook.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './BurningBook.css';
+import BurningBookPic from '../../../images/bookburning.jpg';
 
 export default class BurningBook extends Component {
   render() {
     return (
       <div className="burning-book">
-        <img src={("../../../images/bookburning.jpg")} alt="book burning" />
+        <img src={BurningBookPic} alt="book burning" />
         <h1>THIS BOOK SUCKS!</h1>
         <h3>Is your to-read list too long?
           <br />
